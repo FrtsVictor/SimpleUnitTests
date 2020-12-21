147be28c6c3ecfa6ec89275fa3d2606d86020268
FrtsVictor <victorfreitas.job@gmail.com>
2020-12-21 17:55:38 -0300

    Add productById query

--- UnitTests/Java/DataJpaTest/src/main/java/com/restapi/demo/repository/ProductRepository.java
+++ UnitTests/Java/DataJpaTest/src/main/java/com/restapi/demo/repository/ProductRepository.java
@@ -1,7 +1,18 @@
 package com.restapi.demo.repository;
 
 import com.restapi.demo.model.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ProductRepository  extends CrudRepository<Product, Long> {
+public interface ProductRepository  extends JpaRepository<Product, Long> {
+
+    @Query(
+            value = "SELECT * FROM products pdt WHERE pdt.id = ?1",
+            nativeQuery = true)
+    Product productById(Long id);
+
+    Product findByName(String name);
+
+
+
 }
