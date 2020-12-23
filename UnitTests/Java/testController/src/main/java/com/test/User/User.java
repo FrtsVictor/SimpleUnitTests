a391beff5b46b24b6ff81b2ce16cb270712fc69e
FrtsVictor <victorfreitas.job@gmail.com>
2020-12-22 23:50:08 -0300

    Add User

---
+++ UnitTests/Java/testController/src/main/java/com/test/User/User.java
@@ -0,0 +1,27 @@
+package com.test.User;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 64, nullable = false)
+    private String password;
+
+    @Column(length = 64, nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, length = 64)
+    private String photos;
+}
